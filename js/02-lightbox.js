d3b2716e989613e92373896c7473da8e025a91c7
Gabriel Tur <tur.gabriel90@gmail.com>
2023-03-15 19:14:25 +0100

    HW 7 finish

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,7 +6,8 @@ console.log(galleryItems);
 const gallery = document.querySelector(".gallery");
 
 galleryItems.forEach((item) => {
-  const galleryItem = `<div ><a class="gallery__item" href="${item.oryginal}"><img class="gallery__image" src="${item.preview}" alt="${item.description}"/></a></div>`;
+  const galleryItem = `<div ><a class="gallery__item" href="${item.original}"><img class="gallery__image" src="${item.preview}" 
+  alt="${item.description}"/></a></div>`;
   gallery.insertAdjacentHTML("beforeend", galleryItem);
 });
 
